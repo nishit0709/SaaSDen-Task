117789d8b7289169c0b24c9d86fb430e5fc46e5e
Nishit Pandit <nishitpandit@ymail.com>
2022-06-13 10:51:27 +0530

    Added password hash and session cookies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const userModel = require('./models/user')
+const bcrypt = require('bcrypt')
+
 
 const mongoose = require('mongoose')
 const mongoURL = "mongodb+srv://nishit:AWiHJVPVZFndQxvO@cluster0.rq3kt.mongodb.net/?retryWrites=true&w=majority"
@@ -15,14 +17,22 @@ mongoose
   });
 
 
+const cookieParser = require("cookie-parser")
+const sessions = require('express-session')
+const duration = 1000 * 60 * 15
 
-
-
-
+app.use(sessions({
+    secret: "HighlysecretSauce",
+    saveUninitialized: true,
+    cookie : {maxAge: duration},
+    resave : false
+}))
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+app.use(cookieParser());
+
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -34,31 +44,36 @@ app.get('/',(req, res) =>{
 })
 
 app.post('/', async (req,res) =>{
+    console.log("Login request received")
     const user = await userModel.findOne({userName : req.body.userName})
-    
     if(user == null)
         res.send("User does not exist")
-    else if(user.passWord != req.body.passWord)
-        res.send("Invalid Password")
-    else 
-        res.send("Successfully Logged in")
+
+    const match = await bcrypt.compare(req.body.passWord, user.passWord)
     
-        console.log("Login request received")
+    if(match){
+        req.session.userName = user.userName
+        res.send("Successfully Logged in")
+    }
+    else
+        res.send("Invalid Password")   
 })
 
 app.post('/signup', async(req,res) =>{
-    const doc = new userModel({
-        userName : req.body.userName,
-        passWord : req.body.passWord,
-        email : req.body.email
-    })
-    await doc.save()
     console.log("signup request received")
+    bcrypt.hash(req.body.passWord, 10, (err, hash) =>{
+        const doc = new userModel({
+            userName : req.body.userName,
+            passWord : hash,
+            email : req.body.email
+        })
+        await doc.save()
+    })
 })
 
 app.get('/home', (req,res) => {
     //cookie goes here
-    const user = userModel.findOne({userName: req.body.userName})
+    const user = userModel.findOne({userName: req.session.userName})
     data = {
         userName : user.userName,
         notes : user.notes
@@ -69,7 +84,7 @@ app.get('/home', (req,res) => {
 app.post('/home', (req,res) =>{
     console.log("Note post request received")
 
-    const user = req.body.userName //cookie goes here
+    const user = req.session.userName //cookie goes here
     const note = {
         heading : req.body.heading,
         tags : req.body.tags,
