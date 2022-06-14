6cda7ff2a77a5f01cfb0d9129be8bd0967153f1c
Nishit Pandit <nishitpandit@ymail.com>
2022-06-14 13:52:08 +0530

    fixed bugs and added routes

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const path = require('path')
 const app = express();
-const userModel = require('./models/user')
-const bcrypt = require('bcrypt')
-
 
 const mongoose = require('mongoose')
 const mongoURL = "mongodb+srv://nishit:AWiHJVPVZFndQxvO@cluster0.rq3kt.mongodb.net/?retryWrites=true&w=majority"
@@ -33,79 +30,26 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(cookieParser());
 
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-app.get('/',(req, res) =>{
-    res.render('login')
-})
-
-app.post('/', async (req,res) =>{
-    console.log("Login request received")
-    const user = await userModel.findOne({userName : req.body.userName})
-    if(user == null)
-        res.send("User does not exist")
-
-    const match = await bcrypt.compare(req.body.passWord, user.passWord)
-    
-    if(match){
-        req.session.userName = user.userName
-        res.send("Successfully Logged in")
-    }
+function isUserLogged(req, res, next){
+    if(req.session.ID)
+        next()
     else
-        res.send("Invalid Password")   
-})
+        res.redirect("/")
+}
 
-app.post('/signup', async(req,res) =>{
-    console.log("signup request received")
-    bcrypt.hash(req.body.passWord, 10, (err, hash) =>{
-        const doc = new userModel({
-            userName : req.body.userName,
-            passWord : hash,
-            email : req.body.email
-        })
-        await doc.save()
-    })
-})
 
-app.get('/home', (req,res) => {
-    //cookie goes here
-    const user = userModel.findOne({userName: req.session.userName})
-    data = {
-        userName : user.userName,
-        notes : user.notes
-    }   
-    res.render('home', data)
-})
 
-app.post('/home', (req,res) =>{
-    console.log("Note post request received")
 
-    const user = req.session.userName //cookie goes here
-    const note = {
-        heading : req.body.heading,
-        tags : req.body.tags,
-        body : req.body.body
-    }
 
-    userModel.findOneAndUpdate(
-        {userName: user},
-        {
-            $push : {
-                notes: note
-            }
-        },
-        (err, result) => {
-            if(err)
-                console.log("Error Pushing to the DB")
-            else
-                console.log("Note Succesfully Logged")
-        });
-})
+const login = require('./routes/login')
+const home = require("./routes/home")
+
+app.use("/", login)
+app.use("/home", isUserLogged, home)
+
 
 const port=process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
-
